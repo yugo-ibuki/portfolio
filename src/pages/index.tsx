ccaebeb42c32143f5d7a0230c37bbddbcb0b14c9
yugo-ibuki <y.ibuki91@gmail.com>
2022-04-19 00:04:11 +0900

    feat: indexページ作成途中

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,51 @@
 import type { NextPage } from 'next'
+import Image from 'next/image'
+import { Title } from '@components/Title'
+import { Block } from '@components/Block'
+
+type TProfiles = {
+  name: string
+  description: string
+}
+
+const profiles: TProfiles[] = [
+  { name: 'Name', description: 'YUGO IBUKI' },
+  { name: 'Age', description: '30' },
+  { name: 'Language', description: 'JP / EN' },
+]
 
 const Home: NextPage = () => {
-
   return (
     <main>
-      YUGO IBUKI
+      <div className={'mt-[30px] flex items-center'}>
+        <figure className={'w-[60%] h-[300px] flex justify-center'}>
+          <Image
+            src="/assets/me.jpeg"
+            alt="Picture of the author"
+            className={'ml-auto'}
+            width={300}
+            height={300}
+          />
+        </figure>
+        <div className={'gap-x-5 w-[40%]'}>
+          <dl>
+            {
+              profiles.map(profile => {
+                return (
+                  <>
+                    <dt className={'float-left mr-2'}>{profile.name}:</dt>
+                    <dd>{profile.description}</dd>
+                  </>
+                )
+              })
+            }
+          </dl>
+        </div>
+      </div>
+
+      <Block>
+        <Title>BACKGROUND</Title>
+      </Block>
     </main>
   )
 }
