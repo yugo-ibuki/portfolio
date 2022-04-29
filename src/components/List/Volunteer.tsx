ab088b0fa11d1b53c76b42076c592ac056112577
yugo-ibuki <y.ibuki91@gmail.com>
2022-04-30 00:14:39 +0900

    chore: hover時の挙動追加

--- src/components/List/Volunteer.tsx
+++ src/components/List/Volunteer.tsx
@@ -23,7 +23,7 @@ export const Volunteer: FC = () => {
                 <dd className={'ml-[30px]'}>{v.content}</dd>
                 <dd className={'ml-[30px]'}>
                   <Link href="https://www.kisfvf.com/">
-                    <a target={'_blank'} className={'text-cyan-600'}>
+                    <a target={'_blank'} className={'text-cyan-600 hover:cursor-pointer'}>
                       Kyoto International Student Film & Video Festival<br />
                       (京都国際学生映画祭)
                     </a>
