2780c8b5ed0f82212040e1b5c3f0bad9d2906a64
yugo-ibuki <67675670+yugo-ibuki@users.noreply.github.com>
2022-04-30 22:08:30 +0900

    feat: メール機能追加 (#22)
    * feat: メール機能追加
    
    * chore: formにエラーログ追加
    
    * fix: エラーハンドリング修正
    
    * chore: ログを差し込み
    
    * chore: メッセージ変更
    
    * fix: メール内容変更 && メールを自分に送信するように修正

---
+++ src/pages/api/send.ts
@@ -0,0 +1,45 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import type { ResponseError } from '@sendgrid/helpers/classes'
+
+const handler = (req: NextApiRequest, res: NextApiResponse) => {
+  if(req.method === 'POST') {
+    const sgMail = require('@sendgrid/mail')
+    sgMail.setApiKey(process.env.NEXT_PUBLIC_SENDGRID_KEY)
+    const body = JSON.parse(req.body)
+
+    const msgToSender = {
+      to: body.email,
+      from: 'support@y-ibuki91.app',
+      subject: 'お問合せありがとうございました。',
+      text: 'お問合せを受け付けました。返答をお待ちください。\r\n' + body.message,
+      html: 'お問合せを受け付けました。返答をお待ちください。\r\n' + body.message,
+    }
+    const msgToMe = {
+      to: process.env.NEXT_PUBLIC_MY_EMAIL,
+      from: body.email,
+      subject: 'お問合せありがとうございました。',
+      text: '' +
+        'お問合せを受け付けました。\r\n' +
+        '名前: ' + body.name + '\r\n' +
+        'メールアドレス: ' + body.email + '\r\n' +
+        '所属: ' + body.email + '\r\n' +
+        '内容: ' + body.message + '\r\n',
+      html: 'お問合せを受け付けました。回答をお待ちください。\r\n' + body.message,
+    }
+    ;(async () => {
+      try {
+        await sgMail.send(msgToSender)
+        await sgMail.send(msgToMe)
+      } catch (error) {
+        const err = error as ResponseError
+        if (err.response) {
+          console.error(err.response.body)
+        }
+      }
+    })()
+  }
+
+  res.status(200)
+}
+
+export default handler
