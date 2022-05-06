ee53ca1a73a420050d349aa2ab47e95815498a8f
yugo-ibuki <67675670+yugo-ibuki@users.noreply.github.com>
2022-05-06 15:31:15 +0900

    release (#32)
    * chore: メール本文を英語に対応
    
    * feat: worksページ追加
    
    * chore: aタグホバー時に透過処理追加

--- src/components/List/Work.tsx
+++ src/components/List/Work.tsx
@@ -1,11 +1,42 @@
 import type { FC } from 'react'
+import Link from 'next/link'
+
+const works: {
+  name: string
+  description: string
+  url: string
+  photo: string
+}[] = [
+  {
+    name: 'Japanese population graph by prefecture',
+    description: 'It shows population in Japan by prefecture.',
+    url: 'https://population-app.vercel.app/',
+    photo: '/assets/population-app.png',
+  },
+]
 
 export const Work: FC = () => {
   return (
-    <div>
-      <div>
-        <h3>Works</h3>
-      </div>
-    </div>
+    <ul className={'w-[90%] mx-auto flex flex-col gap-y-[20px]'}>
+      {
+        works.map(w => {
+          return (
+            <li key={w.name}>
+              <dl className={'flex flex-col gap-y-[10px]'}>
+                <dt>
+                  <Link href={w.url}>
+                    <a className={'text-cyan-600 flex flex-col gap-y-3'}>
+                      {w.name}
+                      <img src={w.photo} alt={w.name} />
+                    </a>
+                  </Link>
+                </dt>
+                <dd className={'ml-[30px]'}>{w.description}</dd>
+              </dl>
+            </li>
+          )
+        })
+      }
+    </ul>
   )
 }
