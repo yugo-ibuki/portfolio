92a9b18d274c2847907fb3ac8d986d63c9372099
yugo-ibuki <67675670+yugo-ibuki@users.noreply.github.com>
2022-06-08 00:34:46 +0900

    certificate追加 (#35)
    * chore: メール本文を英語に対応
    
    * feat: worksページ追加
    
    * chore: aタグホバー時に透過処理追加
    
    * fix: メールタイトル修正
    
    * certificate areaを作成 (#34)
    
    * certificate areaを作成
    
    * 不要なimport削除

---
+++ src/components/List/Certificate.tsx
@@ -0,0 +1,40 @@
+import type { FC } from 'react'
+
+const certificates: {
+  name: string
+  lank: string | number
+}[] = [
+  {
+    name: 'TOEIC',
+    lank: 875
+  },
+  {
+    name: 'EIKEM (English Test in Japan)',
+    lank: 'Grade Pre-1'
+  },
+  {
+    name: 'IELTS',
+    lank: '6.0'
+  }
+]
+
+export const Certificate: FC = () => {
+  return (
+    <ul className={'w-[90%] mx-auto flex flex-col gap-y-[20px]'}>
+      {
+        certificates.map(certificate => {
+          return (
+            <li key={certificate.name}>
+              <dl className={'flex flex-col gap-y-[10px]'}>
+                <dt className={'font-bold'}>{certificate.name}</dt>
+                <dd className={'ml-[30px]'}>
+                  {certificate.lank}
+                </dd>
+              </dl>
+            </li>
+          )
+        })
+      }
+    </ul>
+  )
+}
