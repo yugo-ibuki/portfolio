fa113a99d4e3a16a5c940d22b881d3074bbecd78
yugo-ibuki <y.ibuki91@gmail.com>
2023-04-23 19:47:22 +0900

    useLayoutEffectのwarning削除

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import '../style/global.css'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
@@ -6,6 +7,9 @@ import { theme } from '../config/chakra'
 import { Header } from '@components/Layout/Header'
 import { LayoutWrapper } from '@components/Layout/LayoutWrapper'
 import { Analytics } from '@vercel/analytics/react'
+
+// https://stackoverflow.com/questions/58070996/how-to-fix-the-warning-uselayouteffect-does-nothing-on-the-server
+React.useLayoutEffect = React.useEffect
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
