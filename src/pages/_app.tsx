bec9d7196c55e46c01f1bb17806e8815b80f3cb6
yugo-ibuki <y.ibuki91@gmail.com>
2023-04-23 19:32:26 +0900

    不要なimportも削除

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,10 @@
 import '../style/global.css'
-import type { AppContext, AppProps } from 'next/app'
+import type { AppProps } from 'next/app'
 import Head from 'next/head'
 import { ChakraProvider } from '@chakra-ui/react'
 import { theme } from '../config/chakra'
 import { Header } from '@components/Layout/Header'
 import { LayoutWrapper } from '@components/Layout/LayoutWrapper'
-import App from 'next/app'
 import { Analytics } from '@vercel/analytics/react'
 
 function MyApp({ Component, pageProps }: AppProps) {
