1b506be49cdd20758b74cf09c37556aa4eb9789d
yugo-ibuki <y.ibuki91@gmail.com>
2023-10-28 22:45:41 +0900

    mail to new

--- src/lib/sendMail.ts
+++ src/lib/sendMail.ts
@@ -1,3 +1,10 @@
+'use server'
+
+import * as sgMail from '@sendgrid/mail'
+import { contentToSender } from '@lib/mailTemplate/toSender'
+import { contentToMe } from '@lib/mailTemplate/toMe'
+import { NextResponse } from 'next/server'
+
 export interface IFormInputs {
   name: string
   belonging: string
@@ -6,22 +13,41 @@ export interface IFormInputs {
 }
 
 export const sendMail = async (data: IFormInputs) => {
+  sgMail.setApiKey(process.env.NEXT_PUBLIC_SENDGRID_KEY as string)
+
+  const msgToSender = makeMailSenderObject(data)
+  const msgToMe = makeToMeMailObject(data)
   try {
-    const res = await fetch('/api/send', {
-      body: JSON.stringify({
-        name: data.name,
-        email: data.email,
-        belonging: data.belonging,
-        message: data.content
-      }),
-      headers: {
-        'contentType': 'application/json'
-      },
-      method: 'POST'
-    })
-    console.log('メールが送信されました。')
-    console.log(res.status)
-  } catch(e) {
-    console.error(e)
+    await sgMail.send(msgToSender)
+    await sgMail.send(msgToMe)
+  } catch (error) {
+    console.error(error)
+    throw error
   }
+
+  await NextResponse.json({ status: 'ok' })
 }
+
+const makeMailSenderObject = (data: IFormInputs) => ({
+  to: data.email,
+  from: 'support@y-ibuki91.app',
+  subject: 'Thank you for contacting',
+  text: contentToSender({
+    name: data.name,
+    email: data.email,
+    belonging: data.belonging,
+    message: data.content,
+  }),
+})
+
+const makeToMeMailObject = (data: IFormInputs) => ({
+  to: process.env.NEXT_PUBLIC_MY_EMAIL,
+  from: 'support@y-ibuki91.app',
+  subject: 'お問合せがありました。',
+  text: contentToMe({
+    name: data.name,
+    email: data.email,
+    belonging: data.belonging,
+    message: data.content,
+  }),
+})
