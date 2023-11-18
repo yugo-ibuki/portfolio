26fc14352562187022058982e4f47a1e88edbd7c
yugo-ibuki <y.ibuki91@gmail.com>
2023-11-18 13:02:04 +0900

    added components for individual side work

---
+++ src/app/background/(background-individual)/01/page.tsx
@@ -0,0 +1,20 @@
+import { Title, Description } from '@components'
+
+/**
+ * English Mentor Page
+ */
+const Page = () => {
+  return (
+    <main>
+      <Title>English Mentor</Title>
+      <Description subtitle="Tech Commit">
+        I've worked as an English Mentor.
+        <br />
+        I taught the English expression especially for writing, speaking, how to learn English.
+        <br />I also wrote some article about learning English.
+      </Description>
+    </main>
+  )
+}
+
+export default Page
---
+++ src/app/background/(background-individual)/02/page.tsx
@@ -0,0 +1,25 @@
+import { Title, Description } from '@components'
+
+/**
+ * Programming Mentor Page
+ */
+const Page = () => {
+  return (
+    <main>
+      <Title>English Mentor</Title>
+      <Description subtitle="Tech Commit">
+        I've worked as a Laravel Mentor.
+        <br />
+        Usually, writing PHP, how to setup the local developing environment, using MySQL, Laravel,
+        Docker.
+        <br />
+        I also reviewed the DB design, depending on their original app that they plan to build.
+        <br />
+        Taught what the concept of Class, Interface, Object Oriented Programming are.
+        <br />
+      </Description>
+    </main>
+  )
+}
+
+export default Page
