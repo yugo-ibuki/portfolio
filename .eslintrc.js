b3717066df8a3c35697e38177ca04496140e9f43
yugo-ibuki <y.ibuki91@gmail.com>
2023-11-23 12:32:38 +0900

    added works

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,49 +1,44 @@
 module.exports = {
   root: true,
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   plugins: ['prettier'],
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-  ],
+  extends: ['plugin:@typescript-eslint/recommended'],
   rules: {
-    'semi': ['error', 'never', { beforeStatementContinuationChars: 'never' }],
+    semi: ['error', 'never', { beforeStatementContinuationChars: 'never' }],
     'semi-spacing': ['error', { after: true, before: false }],
     'semi-style': ['error', 'first'],
     'no-extra-semi': 'error',
     'no-unexpected-multiline': 'error',
     'no-unreachable': 'error',
-    'quotes': ['error', 'single'],
+    // 'quotes': ['error', 'single'],
     // @typescript-eslint
-    "@typescript-eslint/no-explicit-any": "off",
-    "@typescript-eslint/no-var-requires": "off",
-    "@typescript-eslint/explicit-module-boundary-types": "off",
-    "@typescript-eslint/consistent-type-imports": [
-      "warn",
-      { prefer: "type-imports" },
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/consistent-type-imports': ['warn', { prefer: 'type-imports' }],
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      { varsIgnorePattern: '^_', argsIgnorePattern: '^_' },
     ],
-    "@typescript-eslint/no-unused-vars": [
-      "error",
-      { varsIgnorePattern: "^_", argsIgnorePattern: "^_" },
-    ],
-    "@typescript-eslint/naming-convention": [
-      "error",
-      { selector: ["typeAlias", "typeParameter"], format: ["PascalCase"] },
+    '@typescript-eslint/naming-convention': [
+      'error',
+      { selector: ['typeAlias', 'typeParameter'], format: ['PascalCase'] },
       {
-        selector: "variable",
-        types: ["boolean"],
-        format: ["PascalCase"],
-        prefix: ["no", "is", "has", "should"],
-        filter: { regex: "^_", match: false },
+        selector: 'variable',
+        types: ['boolean'],
+        format: ['PascalCase'],
+        prefix: ['no', 'is', 'has', 'should'],
+        filter: { regex: '^_', match: false },
       },
     ],
   },
   parserOptions: {
-    project: "./tsconfig.json",
+    project: './tsconfig.json',
     parser: 'babel-eslint',
     sourceType: 'module',
     ecmaVersion: 2020,
     ecmaFeatures: {
-      'tsx': true,
+      tsx: true,
     },
   },
 }
