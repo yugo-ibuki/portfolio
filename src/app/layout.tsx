228ac71b122a45367abf202e32202760980a0cfc
yugo-ibuki <y.ibuki91@gmail.com>
2025-07-16 01:29:56 +0900

    add 3D objects on background

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import { Analytics } from '@vercel/analytics/react'
 import { Providers } from './providers'
 import type { Metadata } from 'next'
 import { Toaster } from '@/components/components/ui/toaster'
+import Background3D from '@/components/Background3D'
 
 type Props = {
   children: ReactNode
@@ -26,8 +27,9 @@ const RootLayout: FC<Props> = ({ children }) => {
     <html lang="en">
       <body>
         <Providers>
+          <Background3D />
           <Header />
-          <div className={'max-w-[700px] sp:max-w-[100%] mx-auto px-5 mb-[60px]'}>
+          <div className={'max-w-[700px] sp:max-w-[100%] mx-auto px-5 mb-[60px] relative z-10'}>
             {children}
             <Analytics />
           </div>
